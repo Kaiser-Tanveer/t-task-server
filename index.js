4ab585ddbd94d935837d37ddabc2bf8badca456b
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2023-02-11 18:37:11 +0600

    items deleting api created successfully

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -20,21 +19,47 @@ const run = async () => {
         // Collections 
         const categoriesCollection = client.db("tTask").collection("categories");
         const productsCollection = client.db("tTask").collection("allProducts");
+        const bookingsCollection = client.db("tTask").collection("bookings");
 
         // Crud Operations 
         // Getting Category Data 
         app.get('/categories', async (req, res) => {
             const result = await categoriesCollection.find({}).toArray();
-            console.log(result);
             res.send(result);
         })
 
-        // Getting Products Data 
+        // Getting Products Data by Category and ascending by price 
         app.get('/allProducts/:name', async (req, res) => {
             const name = req.params.name;
             const filter = { category_name: name };
             const result = await productsCollection.find(filter).sort({ price: 1 }).toArray();
-            console.log(result);
+            res.send(result);
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const details = await productsCollection.findOne(filter);
+            res.send(details);
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        })
+
+        app.get('/myBookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await bookingsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await bookingsCollection.deleteOne(filter);
             res.send(result);
         })
     }
