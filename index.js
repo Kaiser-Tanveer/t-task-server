2278bdd628637958875e7318410762548e5620ea
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2023-02-11 22:25:33 +0600

    jwt secret implemented

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+// jwt verification middleware function 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.WEB_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized Access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+// jwt api 
+app.post('/jwt', (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.WEB_TOKEN_SECRET, { expiresIn: "7d" });
+    res.send({ token });
+})
+
 
 const run = async () => {
     try {
@@ -25,6 +49,11 @@ const run = async () => {
         // Getting Category Data 
         app.get('/categories', async (req, res) => {
             const result = await categoriesCollection.find({}).toArray();
+            res.send(result);
+        })
+
+        app.get('/products', async (req, res) => {
+            const result = await productsCollection.find({}).sort({ price: -1 }).toArray();
             res.send(result);
         })
 
@@ -49,7 +78,7 @@ const run = async () => {
             res.send(result);
         })
 
-        app.get('/myBookings', async (req, res) => {
+        app.get('/myBookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
             const query = { email: email }
             const result = await bookingsCollection.find(query).toArray();
